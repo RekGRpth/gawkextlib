ddd4dfa6b6eb1b03f1129e8d2ada0fcaa9f3e8e4
jkahrs <jkahrs>
2005-03-29 20:57:58 +0000

    Added some files which are needed to convert character encodings which Expat does not know. See extension/encodings/README.

---
+++ extension/xml_enc_handler.c
@@ -0,0 +1,108 @@
+/* put the GNU header here */
+/* 2005-03-27 ST tramms */
+
+#include <ctype.h>
+#include <string.h>
+
+#include <expat.h>
+
+#include "encoding.h"
+
+#include "xml_enc_tables.inc"
+#include "xml_enc_registry.inc"
+
+/* the following code is copied from Perl Expat.xs and is
+   written originally by:
+    Larry Wall <larry@wall.org>
+    Clark Cooper <coopercc@netheaven.com> 
+*/
+
+/*================================================================
+** This is the function that expat calls to convert multi-byte sequences
+** for external encodings. Each byte in the sequence is used to index
+** into the current map to either set the next map or, in the case of
+** the final byte, to get the corresponding Unicode scalar, which is
+** returned.
+*/
+
+static int
+convert_to_unicode(void *data, const char *seq) {
+  Encinfo *enc = (Encinfo *) data;
+  PrefixMap *curpfx;
+  int count;
+  int index = 0;
+
+  for (count = 0; count < 4; count++) {
+    unsigned char byte = (unsigned char) seq[count];
+    unsigned char bndx;
+    unsigned char bmsk;
+    int offset;
+
+    curpfx = &enc->prefixes[index];
+    offset = ((int) byte) - curpfx->min;
+
+    if (offset < 0)
+      break;
+    if (offset >= curpfx->len && curpfx->len != 0)
+      break;
+
+    bndx = byte >> 3;
+    bmsk = 1 << (byte & 0x7);
+
+    if (curpfx->ispfx[bndx] & bmsk) {
+      index = enc->bytemap[curpfx->bmap_start + offset];
+    }
+    else if (curpfx->ischar[bndx] & bmsk) {
+      return enc->bytemap[curpfx->bmap_start + offset];
+    }
+    else {
+      break;
+    }
+  }
+  return -1;
+}  /* End convert_to_unicode */
+
+int
+unknownEncoding(void *unused, const char *name, XML_Encoding *info)
+{
+  Encinfo *enc;
+  int namelen;
+  int i;
+  char buff[42];
+    
+  namelen = strlen(name);
+  if (namelen > 40)
+    return XML_STATUS_ERROR;
+    
+  /* Make uppercase */
+  for (i = 0; i < namelen; i++) {
+    buff[i] = toupper(name[i]);
+  }
+  buff[i] = 0;
+
+  /* search the encoding table */
+  i = 0;
+  enc = NULL;
+  while (encs[i].name != 0) {
+    if (strcmp(encs[i].name, buff) == 0) {
+      enc = encs[i].enc;
+      break;
+    }
+    i++;
+  }
+  if (enc == NULL) return XML_STATUS_ERROR; 
+
+  /* install handler */
+  memcpy(info->map, enc->firstmap, 256 * sizeof(int));
+  if (enc->prefixes_size > 0) {
+    info->data = (void *) enc;
+    info->convert = convert_to_unicode;
+  }
+  else {
+    info->data = NULL;
+    info->convert = NULL;
+  }
+  info->release = NULL;
+
+  return XML_STATUS_OK;
+} /* End unknownEncoding */
