c7b64c85328b2ff6ee9ffe53d7438681b4d92fc2
ajschorr <ajschorr>
2006-03-15 16:45:38 +0000

    Wed Mar 15 16:44:06 UTC 2006 Andrew J. Schorr <ajschorr@users.sourceforge.net>
    	* missing_d/snprintf.c: Fix white space and pointer comparisons to
    	match gawk coding style, as per Arnold's request.

--- missing_d/snprintf.c
+++ missing_d/snprintf.c
@@ -33,23 +33,23 @@
    implementations of tmpfile() were not secure. */
 
 static FILE *
-safe_tmpfile(void)
+safe_tmpfile (void)
 {
-   static const char template[] = "/tmp/snprintfXXXXXX";
-   FILE *f;
-   int fd;
-   char t[sizeof(template)];
+	static const char template[] = "/tmp/snprintfXXXXXX";
+	FILE *f;
+	int fd;
+	char t[sizeof (template)];
 
-   strcpy(t,template);
-   if ((fd = mkstemp(t))<0)
-      return NULL;
-   unlink(t);
-   if (!(f = fdopen(fd,"w+b"))) {
-      close(fd);
-      return NULL;
-   }
-   /* setvbuf(f,NULL,_IOFBF,4*BUFSIZ); */
-   return f;  
+	strcpy (t, template);
+	if ((fd = mkstemp (t)) < 0)
+		return NULL;
+	unlink (t);
+	if ((f = fdopen (fd, "w+b")) == NULL) {
+		close (fd);
+		return NULL;
+	}
+	/* setvbuf(f,NULL,_IOFBF,4*BUFSIZ); */
+	return f;
 }
 
 #elif defined(HAVE_TMPFILE)
@@ -59,78 +59,78 @@ safe_tmpfile(void)
 #endif
 
 int
-gawk_vsnprintf(char *restrict buf, size_t len,
-	       const char *restrict fmt, va_list args)
+gawk_vsnprintf (char *restrict buf, size_t len,
+		const char *restrict fmt, va_list args)
 {
-   int actual;
-   int nread;
-   size_t cnt = 0;
+	int actual;
+	int nread;
+	size_t cnt = 0;
 #ifndef SNPRINTF_REENTRANT
-   static
+	static
 #endif
-      FILE *fp;
+	FILE *fp;
 
-   if (!buf || len < 1)
-      return -1;
+	if ((buf == NULL) || (len < 1))
+		return -1;
 
-   buf[0] = '\0'; /* in case the caller does not check the return code! */
+	buf[0] = '\0';	/* in case the caller does not check the return code! */
 
 #ifdef SNPRINTF_REENTRANT
-   if (!(fp = safe_tmpfile()))
-     return -1;
+	if ((fp = safe_tmpfile ()) == NULL)
+		return -1;
 #else
-   if (!fp && !(fp = safe_tmpfile()))
-     return -1;
-   rewind(fp);
+	if ((fp == NULL) && ((fp = safe_tmpfile ()) == NULL))
+		return -1;
+	rewind (fp);
 #endif
-   actual = vfprintf(fp,fmt,args);
-   rewind(fp);
-   if (actual < 0) {
+	actual = vfprintf (fp, fmt, args);
+	rewind (fp);
+	if (actual < 0) {
 #ifdef SNPRINTF_REENTRANT
-      fclose(fp);
+		fclose (fp);
 #endif
-      return -1;
-   }
-   else if ((size_t)actual < len)
-      len = actual;
-   else
-      --len;
-   while (cnt < len && (nread = fread(buf+cnt,1,len-cnt,fp)) > 0)
-      cnt += nread;
-   buf[cnt] = '\0';
+		return -1;
+	}
+	else if ((size_t) actual < len)
+		len = actual;
+	else
+		--len;
+	while (cnt < len && (nread = fread (buf + cnt, 1, len - cnt, fp)) > 0)
+		cnt += nread;
+	buf[cnt] = '\0';
 #ifdef SNPRINTF_REENTRANT
-   fclose(fp);
+	fclose (fp);
 #endif
-   if (cnt < len)
-      return -1;
+	if (cnt < len)
+		return -1;
 
-   return actual;
+	return actual;
 }
 
-int 
+int
 #if defined(HAVE_STDARG_H) && defined(__STDC__) && __STDC__
-gawk_snprintf(char *restrict buf, size_t len, const char *restrict fmt, ...)
+gawk_snprintf (char *restrict buf, size_t len, const char *restrict fmt, ...)
 #else
-gawk_snprintf(va_alist)
-va_dcl
+gawk_snprintf (va_alist)
+     va_dcl
 #endif
 {
-   int rv;
-   va_list args;
+	int rv;
+	va_list args;
 
 #if defined(HAVE_STDARG_H) && defined(__STDC__) && __STDC__
-   va_start(args,fmt);
+	va_start (args, fmt);
 #else
-   char *buf;
-   size_t len;
-   char *fmt;
+	char *buf;
+	size_t len;
+	char *fmt;
 
-   va_start(args);
-   buf = va_arg(args, char *);
-   len = va_arg(args, size_t);
-   fmt = va_arg(args, char *);
+	va_start (args);
+	buf = va_arg (args, char *);
+	len = va_arg (args, size_t);
+	fmt = va_arg (args, char *);
 #endif
-   rv = gawk_vsnprintf(buf,len,fmt,args);
-   va_end(args);
-   return rv;
+	rv = gawk_vsnprintf (buf, len, fmt, args);
+	va_end (args);
+	return rv;
 }
